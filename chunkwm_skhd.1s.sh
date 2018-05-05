7b86a6cc985b7c7b3918a92f1c255a62b55d940d
David Gillies <dave.gillies@gmail.com>
2018-05-04 17:22:26 -0700

    Add options for toggling desktop mode and equalizing windows

--- chunkwm_skhd.1s.sh
+++ chunkwm_skhd.1s.sh
@@ -11,18 +11,34 @@
 # For skhd, see: https://github.com/koekeishiya/skhd
 
 export PATH=/usr/local/bin:$PATH
+CURRENT_MODE=$(chunkc tiling::query --desktop mode)
+case $CURRENT_MODE in
+  'bsp')
+    MODE_TOGGLE='monocle'
+    MODE_EMOJI='⊞'
+    ;;
+  'monocle')
+    MODE_TOGGLE='bsp'
+    MODE_EMOJI='⧈'
+    ;;
+esac
 
 if [[ "$1" = "stop" ]]; then
   brew services stop chunkwm
   brew services stop skhd
-fi
-
-if [[ "$1" = "restart" ]]; then
+elif [[ "$1" = "restart" ]]; then
   brew services restart chunkwm
   brew services restart skhd
+elif [[ "$1" = "toggle" ]]; then
+  chunkc tiling::desktop --layout $MODE_TOGGLE
+elif [[ "$1" = "equalize" ]]; then
+  chunkc tiling::desktop --equalize
+else
+  echo "$(chunkc tiling::query --desktop id):${MODE_EMOJI} | length=5"
+  echo "---"
+  echo "Toggle layout | bash='$0' param1=toggle terminal=false"
+  echo "Equalize windows | bash='$0' param1=equalize terminal=false"
+  echo "---"
+  echo "Restart chunkwm & skhd | bash='$0' param1=restart terminal=false"
+  echo "Stop chunkwm & skhd | bash='$0' param1=stop terminal=false"
 fi
-
-echo "$(chunkc tiling::query --desktop id):$(chunkc tiling::query --desktop mode) | length=5"
-echo "---"
-echo "Restart chunkwm & skhd | bash='$0' param1=restart terminal=false"
-echo "Stop chunkwm & skhd | bash='$0' param1=stop terminal=false"
