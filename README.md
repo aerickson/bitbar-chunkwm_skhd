1eed6e73a9f108c861701e7fb6bb640f09b17b90
Andrew Erickson <aerickson@gmail.com>
2018-11-15 21:17:39 -0800

    gbit: Merging branch 'new_stuff'.
    Squashed commit of the following:
    
    commit 854864a07158ec1f9345b3b36ce891f4482f7c6f
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Thu Nov 15 21:15:48 2018 -0800
    
        add image of current state
    
    commit fef762dfbe52c27f5848650a08232f9eac082fa3
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Thu Nov 15 21:12:17 2018 -0800
    
        don't trim the bar
    
    commit 48425e64d4fa63f53ded3b69db01f9f117d2169b
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Thu Nov 15 21:09:18 2018 -0800
    
        cleanup and tweaking sleeps
    
    commit 30969af88f9342b6c2364b06d3fb65554990b393
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 22:01:17 2018 -0800
    
        update after changing layout also
    
    commit 5d32c561dad0db6b9c3faa9a2bcfa69f2a60c31d
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 21:52:52 2018 -0800
    
        fire update requests after start/stop/restart
    
        otherwise plugin wasn't updating for 2 minutes with the bitbar-notifier plugin
    
    commit 355bd7b4b6192512f60ac1a62f60ffb8e49f402e
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 03:51:42 2018 -0800
    
        tweak verbiage regarding chunkwm state
    
    commit bdc457b30d09413cd7c03451a861fab7ce40dc05
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 03:49:24 2018 -0800
    
        add c compilation / benchmark tool dir
    
    commit 38146056e928d504ff95b1c956d795921e2e4c5c
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 03:18:08 2018 -0800
    
        todos
    
    commit adcb00ab49d3fca1d707454ea8a77bff82583c63
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 03:16:36 2018 -0800
    
        menu shows display mode
    
    commit cca4ccdbc32a2ce0108fa63b940197345e32ce42
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 03:12:36 2018 -0800
    
        now focused on chunkwm
    
    commit 3bfc1247734073cd87edfff8a5dbe40e29a0334b
    Author: Andrew Erickson <aerickson@gmail.com>
    Date:   Wed Nov 14 02:23:04 2018 -0800
    
        wip
    
    //// Powered by gbit (git branch integration tool).

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bitbar-chunkwm_skhd
 A simple bitbar for [chunkwm](https://github.com/koekeishiya/chunkwm)/[skhd](https://github.com/koekeishiya/skhd). See [here](https://github.com/matryer/bitbar#installing-plugins) for installation.
+
+## changes by aerickson
+
+- focus more on chunkwm (vs skhd)
+
+![current look](https://content.evernote.com/shard/s74/sh/4181b659-9e1b-494d-aebc-a7b825760a10/e876c91950b088e4/res/6403b129-9733-4500-bf1f-215046131ead/skitch.png)
--- chunkwm_skhd.1s.sh
+++ chunkwm_skhd.1s.sh
@@ -10,41 +10,103 @@
 # Info about chunkwm, see: https://github.com/koekeishiya/chunkwm
 # For skhd, see: https://github.com/koekeishiya/skhd
 
+function refreshBB {
+  open -g 'bitbar://refreshPlugin?name=chunkwm.*?.sh'
+}
+
 export PATH=/usr/local/bin:$PATH
-CURRENT_MODE=$(chunkc tiling::query --desktop mode)
+CURRENT_MODE=$(chunkc tiling::query --desktop mode  2>&1)
 case $CURRENT_MODE in
+  'chunkc: connection failed!')
+    CHUNK_STATE='stopped'
+    MODE=''
+    MODE_TOGGLE='none'
+    MODE_EMOJI="⧄"
+    ;;
   'bsp')
+    CHUNK_STATE='running'
+    MODE='bsp'
     MODE_TOGGLE='float'
     MODE_EMOJI='⊞'
     ;;
   'float')
+    MODE='float'
+    CHUNK_STATE='running'
     MODE_TOGGLE='bsp'
     MODE_EMOJI='⧉'
     ;;
+  *)
+    CHUNK_STATE='stopped'
+    ;;
 esac
 
+#
+# command handlers
+#
+# SLEEP_TIME=0.5
+SLEEP_TIME=1
 if [[ "$1" = "stop" ]]; then
   brew services stop chunkwm
   brew services stop skhd
-elif [[ "$1" = "restart" ]]; then
+  refreshBB
+elif [[ "$1" = "start_chunk" ]]; then
+  brew services start chunkwm
+  sleep $SLEEP_TIME
+  refreshBB
+elif [[ "$1" = "restart_chunk" ]]; then
+  brew services restart chunkwm
+  # chunkwm isn't ready for queries right away after restarting, wait a bit
+  # sleep $SLEEP_TIME
+  refreshBB
+elif [[ "$1" = "stop_chunk" ]]; then
+  brew services stop chunkwm
+  refreshBB
+elif [[ "$1" = "restart_both" ]]; then
   brew services restart chunkwm
   brew services restart skhd
+  refreshBB
 elif [[ "$1" = "dfocus" ]]; then
   chunkc core::unload ffm.so
 elif [[ "$1" = "efocus" ]]; then
   chunkc core::load ffm.so
 elif [[ "$1" = "toggle" ]]; then
   chunkc tiling::desktop --layout $MODE_TOGGLE
+  refreshBB
 elif [[ "$1" = "equalize" ]]; then
   chunkc tiling::desktop --equalize
+  refreshBB
 else
-  echo "$(chunkc tiling::query --desktop id):${MODE_EMOJI} | length=5"
+  #
+  # display block
+  #
+  if [[ "$CHUNK_STATE" = "running" ]]; then
+    #echo "c ${MODE_EMOJI}"
+    # TODO: hide display of desktop id behind flag?
+    #echo "c ${MODE_EMOJI} $(chunkc tiling::query --desktop id) | length=5"
+    echo "c ${MODE_EMOJI} $(chunkc tiling::query --desktop id)"
+  else
+    echo "c ${MODE_EMOJI}"
+  fi
   echo "---"
-  echo "Toggle layout | bash='$0' param1=toggle terminal=false"
-  echo "Equalize windows | bash='$0' param1=equalize terminal=false"
-  echo "Enable focus follows mouse | bash='$0' param1=efocus terminal=false" 
-  echo "Disable focus follows mouse | bash='$0' param1=dfocus terminal=false"
-  echo "---"
-  echo "Restart chunkwm & skhd | bash='$0' param1=restart terminal=false"
-  echo "Stop chunkwm & skhd | bash='$0' param1=stop terminal=false"
+  if [[ "$CHUNK_STATE" = "running" ]]; then
+    # TODO: selector for all 3 modes?
+    echo "Desktop Mode: ${MODE}"
+    echo "Toggle Layout | bash='$0' param1=toggle terminal=false"
+    echo "Equalize Windows | bash='$0' param1=equalize terminal=false"
+    # TODO: figure out how to make this a toggle (add query command or detect somehow)
+    echo "Focus Follows Mouse"
+    echo "--Enable FFM | bash='$0' param1=efocus terminal=false" 
+    echo "--Disable FFM | bash='$0' param1=dfocus terminal=false"
+    echo "---"
+  fi
+
+  echo "chunkwm: $CHUNK_STATE"
+  if [[ "$CHUNK_STATE" = "running" ]]; then
+    echo "Restart chunkwm | bash='$0' param1=restart_chunk terminal=false"
+    echo "Stop chunkwm | bash='$0' param1=stop_chunk terminal=false"
+  else
+    echo "Start chunkwm | bash='$0' param1=start_chunk terminal=false"
+  fi
+  # TODO: add back in control of skhd? behind a flag or option?
+
 fi
